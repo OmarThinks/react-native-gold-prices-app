3c71730fa863b1360d30bf5ef38f98f842e58d01
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2026-05-14 01:15:17 +0300

    feat: displayed the add in all the screens

--- expo-app/src/screens/LoginScreen.tsx
+++ expo-app/src/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import BannerAd from "@/components/Views/ads/BannerAd";
 import { setUser } from "@/redux/slices/auth/authSlice";
 import { useColors } from "@/redux/slices/themeSlice/colorsHooks";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
@@ -20,63 +21,69 @@ const LoginScreen = () => {
   };
 
   return (
-    <View
-      className="flex-1 items-center justify-center px-6"
-      style={{ backgroundColor: colors.background }}
-    >
-      {/* Header Section */}
-      <View className="mb-12 items-center">
-        <Text className="text-4xl font-bold" style={{ color: colors.primary }}>
-          Gold Prices
-        </Text>
-        <Text className="mt-2 text-lg" style={{ color: colors.text }}>
-          Get live gold prices
-        </Text>
-      </View>
-
-      {/* Sign In Button */}
-      <TouchableOpacity
-        onPress={handleSignIn}
-        disabled={isLoading}
-        className=" rounded-lg py-4 self-stretch"
-        style={{
-          backgroundColor: isLoading ? colors.secondary : colors.primary,
-          opacity: isLoading ? 0.7 : 1,
-        }}
+    <View className=" self-stretch flex-1">
+      <View
+        className="flex-1 items-center justify-center px-6"
+        style={{ backgroundColor: colors.background }}
       >
-        {isLoading ? (
-          <View className="flex-row items-center justify-center">
-            <ActivityIndicator color={colors.background} size="small" />
-            <Text
-              className="ml-2 text-center text-lg font-semibold"
-              style={{ color: colors.background }}
-            >
-              Signing in...
-            </Text>
-          </View>
-        ) : (
-          <View className=" self-stretch items-center justify-center flex-row gap-2">
-            <Text
-              className="text-center text-lg font-semibold"
-              style={{ color: colors.background }}
-            >
-              Get Started
-            </Text>
-            <MaterialCommunityIcons
-              name="arrow-right-thick"
-              size={24}
-              color={colors.background}
-            />
-          </View>
-        )}
-      </TouchableOpacity>
+        {/* Header Section */}
+        <View className="mb-12 items-center">
+          <Text
+            className="text-4xl font-bold"
+            style={{ color: colors.primary }}
+          >
+            Gold Prices
+          </Text>
+          <Text className="mt-2 text-lg" style={{ color: colors.text }}>
+            Get live gold prices
+          </Text>
+        </View>
 
-      {/* Footer Text */}
-      <View className="mt-8 items-center">
-        <Text className="text-sm" style={{ color: colors.text }}>
-          Sign in anonymously to continue
-        </Text>
+        {/* Sign In Button */}
+        <TouchableOpacity
+          onPress={handleSignIn}
+          disabled={isLoading}
+          className=" rounded-lg py-4 self-stretch"
+          style={{
+            backgroundColor: isLoading ? colors.secondary : colors.primary,
+            opacity: isLoading ? 0.7 : 1,
+          }}
+        >
+          {isLoading ? (
+            <View className="flex-row items-center justify-center">
+              <ActivityIndicator color={colors.background} size="small" />
+              <Text
+                className="ml-2 text-center text-lg font-semibold"
+                style={{ color: colors.background }}
+              >
+                Signing in...
+              </Text>
+            </View>
+          ) : (
+            <View className=" self-stretch items-center justify-center flex-row gap-2">
+              <Text
+                className="text-center text-lg font-semibold"
+                style={{ color: colors.background }}
+              >
+                Get Started
+              </Text>
+              <MaterialCommunityIcons
+                name="arrow-right-thick"
+                size={24}
+                color={colors.background}
+              />
+            </View>
+          )}
+        </TouchableOpacity>
+
+        {/* Footer Text */}
+        <View className="mt-8 items-center">
+          <Text className="text-sm" style={{ color: colors.text }}>
+            Sign in anonymously to continue
+          </Text>
+        </View>
       </View>
+      <BannerAd />
     </View>
   );
 };
